db38de8e1e33ac8800070eb108235fd16d63d3d4
Erfan Taghvaei <etaghvaei0098@gmail.com>
2017-08-21 11:24:00 +0430

    Ok

--- app/src/main/java/com/example/ideapad510/framents/BlankFragment.java
+++ app/src/main/java/com/example/ideapad510/framents/BlankFragment.java
@@ -19,6 +19,8 @@ import android.widget.Toast;
 public class BlankFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    //Comment to check !
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     Button btn1;
